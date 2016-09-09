a24f8ab0c8981758baa2ee589f02c5a2faac6e8d
Deeptesh Chagan <deepc28@gmail.com>
2016-09-09 22:51:22 +1000

    Change to a single tests dir, limit tests path

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import Notifications from 'components/NotificationsContainer/NotificationsContainer'
-import reducer from 'modules/Notifications'
-import {createNotification, removeNotification, removeAllNotifications} from 'modules/Notifications'
+import reducer, {createNotification, removeNotification, removeAllNotifications} from 'modules/Notifications'
 
 export {
     reducer,
