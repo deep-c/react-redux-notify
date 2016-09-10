19f0a682ffbef6384bedd9c8bd38adbe48a6dc5d
Deeptesh Chagan <deepc28@gmail.com>
2016-09-11 01:53:31 +1000

    Export default notification type constants

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
 import Notifications from 'components/NotificationsContainer/NotificationsContainer'
-import reducer, {createNotification, removeNotification, removeAllNotifications} from 'modules/Notifications'
+import reducer, 
+    {createNotification, 
+    removeNotification, 
+    removeAllNotifications,
+    NOTIFICATION_TYPE_SUCCESS,
+    NOTIFICATION_TYPE_WARNING,
+    NOTIFICATION_TYPE_INFO,
+    NOTIFICATION_TYPE_ERROR} from 'modules/Notifications'
 
 export {
     reducer,
     Notifications,
     createNotification,
     removeNotification,
-    removeAllNotifications    
+    removeAllNotifications,
+    NOTIFICATION_TYPE_SUCCESS,
+    NOTIFICATION_TYPE_WARNING,
+    NOTIFICATION_TYPE_INFO,
+    NOTIFICATION_TYPE_ERROR        
 }
 
 export default reducer
